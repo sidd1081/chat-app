6c3b8e1ec80a8ce9b03eeb984e181a52daef8ac4
Siddhant Gupta <siddhant9696gupta761@gmail.com>
2025-08-29 18:44:01 +0530

    project

--- server/index.js
+++ server/index.js
@@ -9,18 +9,10 @@ require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
-
-mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("DB Connetion Successfull");
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+console.log(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("✅ DB Connection Successful"))
+  .catch(err => console.error("❌ DB Error:", err.message));
 
 app.get("/ping", (_req, res) => {
   return res.json({ msg: "Ping Successful" });
